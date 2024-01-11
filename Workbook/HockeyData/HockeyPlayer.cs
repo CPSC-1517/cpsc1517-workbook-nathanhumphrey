9099e87ebcb5fb3e6d84f81d57ed3ed9e40f902d
Nathan Humphrey <NathanH@nait.ca>
2024-01-10 21:26:10 -0700

    Add HockeyPlayer class fields

---
+++ Workbook/HockeyData/HockeyPlayer.cs
@@ -0,0 +1,20 @@
+﻿namespace Hockey.Data
+{
+	public class HockeyPlayer
+	{
+		// Fields
+		private string _birthPlace;
+		private DateOnly _dateOfBirth;
+		private string _firstName;
+		private string _lastName;
+		private int _heightInInches;
+		private int _weightInPounds;
+		private string _position;
+		private string _shot;
+
+		// Properties
+
+
+		// Methods
+	}
+}
