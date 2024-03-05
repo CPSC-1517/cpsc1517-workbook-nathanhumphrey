eaf7d7d0d3b0d3f883721400f9187e74f0356433
Nathan Humphrey <NathanH@nait.ca>
2024-03-04 21:14:06 -0700

    Add WestWindSystem

---
+++ Workbook/WestWindConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+
+WestWindContext context = new WestWindContext();
+
+foreach (var customer in context.Customers)
+{
+	Console.WriteLine(customer.ContactName);
+}
+
+// Run LINQ queries
+
+Customer firstCustomer = context.Customers.First();
+
+Console.WriteLine($"The first customer is: {firstCustomer.ContactName}");
