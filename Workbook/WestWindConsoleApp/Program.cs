317bfcbd4f79c7791e492357fb55008ec0b7e9ec
Nathan Humphrey <NathanH@nait.ca>
2024-03-11 20:12:22 -0600

    Update WWSystem Add CustomerServices

--- Workbook/WestWindConsoleApp/Program.cs
+++ Workbook/WestWindConsoleApp/Program.cs
@@ -1,8 +1,23 @@
-﻿using WestWindSystem.DAL;
+﻿using WestWindSystem.BLL;
+using WestWindSystem.DAL;
 using WestWindSystem.Entities;
 
 WestWindContext context = new WestWindContext();
 
+// Using a services class
+CustomerServices customerServices = new CustomerServices(context);
+
+foreach (var customer in customerServices.GetAllCustomers())
+{
+	Console.WriteLine(customer.ContactName);
+}
+
+Customer? maria = customerServices.GetCustomerByContactName("maria");
+
+Console.WriteLine($"Maria works for: {maria.CompanyName}");
+
+// Using RAW context
+/*
 foreach (var customer in context.Customers)
 {
 	Console.WriteLine(customer.ContactName);
@@ -10,6 +25,9 @@ foreach (var customer in context.Customers)
 
 // Run LINQ queries
 
-Customer firstCustomer = context.Customers.First();
+// Search by partial contact name
+Customer firstCustomer = context.Customers.Where(c => c.ContactName.Contains("m")).FirstOrDefault();
+
 
 Console.WriteLine($"The first customer is: {firstCustomer.ContactName}");
+*/
---
+++ Workbook/WestWindSystem/BLL/CustomerServices.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Required namespaces
+using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+
+namespace WestWindSystem.BLL
+{
+	public class CustomerServices
+	{
+		private readonly WestWindContext _context;
+
+		// TODO: change to internal
+		public CustomerServices(WestWindContext context)
+		{
+			_context = context;
+		}
+
+		/// <summary>
+		/// blah blah
+		/// </summary>
+		/// <returns></returns>
+		public List<Customer> GetAllCustomers()
+		{
+			return _context.Customers.ToList();
+		}
+
+		public Customer? GetCustomerById(string id) 
+		{
+			return _context.Customers.Where(c => c.CustomerId == id).FirstOrDefault();
+		}
+
+		public Customer? GetCustomerByContactName(string contactName) 
+		{
+			contactName = contactName.ToLower();
+			Customer? customer = _context.Customers.Where(c => c.ContactName.ToLower().Contains(contactName)).FirstOrDefault();
+			return customer;
+		}
+
+	}
+}
